92ab8048e1f76341ad5c265a83a52d14dda29813
DarthLuisDa <5cvargas.luis@gmail.com>
2023-10-29 22:27:05 -0600

    Declaracion de Objetos

--- .vscode/objetos.js
+++ .vscode/objetos.js
@@ -3,9 +3,10 @@
 /* let persona = {}
 console.log(persona) */
 
+//Primera Forma de declarar un objeto
 //Objeto persona con propiedades
 
-let persona = {
+/* let persona = {
     nombre: 'Daniel',
     apellido: 'Vargas',
     edad: 26,
@@ -13,4 +14,27 @@ let persona = {
     hijos: false
 }
 
-console.log(persona)
+console.log(persona.apellido) */
+
+//Segunda Forma de declarar un objeto
+
+/* let persona = new Object()
+persona.nombre = 'Daniel',
+persona.apellido = 'Vargas',
+persona.edad = 26,
+
+console.log(persona.apellido) */
+
+//Tercera Forma de declarar un objeto
+
+let persona = new Object()
+persona["nombre"] = 'Daniel',
+persona["apellido"] = 'Vargas',
+persona["edad"] = 26
+persona.pais = 'Inglaterra'
+delete persona.pais
+
+//console.log(persona)
+console.log(`Mi nombre es ${persona.nombre} y mi apellido es ${persona.apellido}`)
+console.log("Mi nombre es" + " "+ persona.nombre + " " + "y mi apellido es" + " " + persona.apellido)
+
