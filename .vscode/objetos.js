88327a7f38728177e555c5116b0afd98d3998e58
DarthLuisDa <5cvargas.luis@gmail.com>
2023-10-29 21:04:30 -0600

    Se agrego una carpeta general .vscode y una de objetos.js

---
+++ .vscode/objetos.js
@@ -0,0 +1,16 @@
+//Objeto declarado sin propiedades
+
+/* let persona = {}
+console.log(persona) */
+
+//Objeto persona con propiedades
+
+let persona = {
+    nombre: 'Daniel',
+    apellido: 'Vargas',
+    edad: 26,
+    estudios:['Licenciatura'],
+    hijos: false
+}
+
+console.log(persona)
